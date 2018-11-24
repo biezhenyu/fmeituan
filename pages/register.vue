3c6e2868eda978e55e284f0a96379babbd7d7789
biezhenyu <biezhenyu521@163.com>
2018-11-24 14:06:20 +0800

    新建公共模板

---
+++ pages/register.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'blank',
+}
+</script>
+
+<style lang="scss">
+@import "~/assets/css/register/index.scss";
+</style>
